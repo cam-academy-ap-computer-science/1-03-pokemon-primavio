658856cf484fc47df308ef4893108581168280a6
primavio000 <primavio000@CAM-LAB-22.bgsd.k12.wa.us>
2019-09-09 07:27:24 -0700

    Finished Pokemon

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -3,7 +3,11 @@ public class Pokemon {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Pikachu welcoms you to the world of Pokemon!)");
+		System.out.println(" (\\__/)");
+		System.out.println(" (o^.^)");
+		System.out.println("z(_(\")(\")");
+	
 	}
 
 }
